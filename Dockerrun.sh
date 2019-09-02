02eac1661a547a94183ac63f8f7c335038b741c7
ipkstef <stefano.amanuel@me.com>
2019-09-01 23:16:52 -0400

    went with bash

---
+++ Dockerrun.sh
@@ -0,0 +1,3 @@
+docker run -d -v home/stefanos/Alliance/data:/data -e VERSION=1.12.2 \
+  -e TYPE=FORGE  -e FORGEVERSION=14.23.5.2768 \
+  -p 25565:25565 -e EULA=TRUE --name alliance itzg/minecraft-server 
