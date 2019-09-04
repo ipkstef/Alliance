d9917dccd3bcb235d6c2d624213cf055a192ed0d
ipkstef <stefano.amanuel@me.com>
2019-09-03 22:38:12 -0400

    changed memory to 3gigs

--- Dockerrun.sh
+++ Dockerrun.sh
@@ -1,3 +1,3 @@
 docker run -d -v ./data:/data -e VERSION=1.12.2 \
-  -e TYPE=FORGE  -e FORGEVERSION=14.23.5.2768 \
+  -e TYPE=FORGE  -e FORGEVERSION=14.23.5.2768 -e MEMORY=3G \
   -p 25565:25565 -e EULA=TRUE --name alliance itzg/minecraft-server 
